5fcefab90b85c542f8612b53f364cb55dc5a4028
dangphanduy <117800872+dangphanduy@users.noreply.github.com>
2025-07-02 15:15:37 +0700

    Update

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class WaveConfigSo : ScriptableObject
+    {
+        
+    }
+}
